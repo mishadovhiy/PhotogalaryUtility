5273c8488b0af00955af847b6651680b157f7920
Misha Dovhiy <44978117+mishadovhiy@users.noreply.github.com>
2026-01-11 05:26:10 +0200

    first commit

---
+++ PhotoGalaryUtility/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoGalaryUtility
+//
+//  Created by Mykhailo Dovhyi on 11.01.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
